77a3106243138647b0d895af4082ec6c3e752ae8
Tiago Pires <621869+tiagogalvao@users.noreply.github.com>
2021-05-31 10:42:38 -0300

    Adjusting to run under Windows without issues QOL improvements

--- wifite/attack/all.py
+++ wifite/attack/all.py
@@ -6,7 +6,6 @@ from .wpa import AttackWPA
 from .wps import AttackWPS
 from .pmkid import AttackPMKID
 from ..config import Configuration
-from ..exceptions.timeoutException import TimeoutException
 from ..util.color import Color
 from ..model.target import WPSState
 
@@ -104,9 +103,9 @@ class AttackAll(object):
             except Exception as e:
                 Color.pexception(e)
                 continue
-            except TimeoutException(e):
-                Color.pexception(e)
-                continue
+            # except TimeoutException(e):
+            #     Color.pexception(e)
+            #     continue
             except KeyboardInterrupt:
                 Color.pl('\n{!} {O}Interrupted{W}\n')
                 answer = cls.user_wants_to_continue(targets_remaining, len(attacks))
